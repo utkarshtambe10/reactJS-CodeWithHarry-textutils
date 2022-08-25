ed824e0c390e380becb87156787c1db6221fe9cb
Utkarsh Tambe <utkarsh.tambe33@gmail.com>
2022-08-25 10:43:02 +0530

    creating dark-mode button and its utility

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
+//import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
       <Navbar title="TextUtils" aboutText="About TextUtils"/>
-      <TextForm heading="Enter the text here to analyze"/>
+      {/*<TextForm heading="Enter the text here to analyze"/>*/}
+      <About/>
     </>
   );
 }
