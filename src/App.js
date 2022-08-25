738a0ad60d8678d934963162f576c356d5d02179
Utkarsh Tambe <utkarsh.tambe33@gmail.com>
2022-08-25 11:13:08 +0530

    adding more logic buttons

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
-import About from './components/About';
+//import About from './components/About';
 import Navbar from './components/Navbar';
-//import TextForm from './components/TextForm';
+import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
       <Navbar title="TextUtils" aboutText="About TextUtils"/>
-      {/*<TextForm heading="Enter the text here to analyze"/>*/}
-      <About/>
+      <TextForm heading="Enter the text here to analyze"/>
+      {/*<About/>*/}
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,39 +1,61 @@
 import React, { useState } from 'react'
 
 export default function TextForm(props) {
-    const handleUpClick =()=> {
+    const [text, setText] = useState('');  //state
+
+    const handleUpClick = () => {
         let newText = text.toUpperCase();
         setText(newText);
     }
 
-    const handleLoClick =()=> {
+    const handleLoClick = () => {
         let newText = text.toLowerCase();
         setText(newText);
     }
 
-    const handleOnChange =(event)=> {
+    const handleClear = () => {
+        let newText = ' ';
+        setText(newText);
+    }
+    
+    //credits: A
+    const handleCopy = () => {
+        let text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+
+    //credits: coding wala
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+    }
+
+    const handleOnChange = (event) => {
         setText(event.target.value);
     }
 
-    const [text, setText] = useState('');  //state
-
     return (
-        <>        
-        <div className="container my-3">
-            <h1>{props.heading}</h1>
+        <>
+            <div className="container my-3">
+                <h1>{props.heading}</h1>
                 <div class="form-group">
                     <textarea class="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
                 </div>
                 <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
-                <button className= "btn btn-primary mx-2" onClick={handleLoClick}> Convert to Lowercase</button>
-        </div>
-        <div className="container my-3">
-            <h1>Your text summary</h1>
-            <p>{text.split(" ").length} words and {text.length} characters</p>
-            <p>{0.008 * text.split(" ").length} minutes to read above text</p>
-            <h2>Preview</h2>
-            <p>{text}</p>
-        </div>
+                <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
+                <button className="btn btn-primary mx-2" onClick={handleClear}>Clear Text</button>
+                <button className="btn btn-primary mx-2" onClick={handleCopy}>Copy Text</button>
+                <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+                
+            </div>
+            <div className="container my-3">
+                <h1>Your text summary</h1>
+                <p>{text.split(" ").length} words and {text.length} characters</p>
+                <p>{0.008 * text.split(" ").length} minutes to read above text</p>
+                <h2>Preview</h2>
+                <p>{text}</p>
+            </div>
         </>
     )
 }
