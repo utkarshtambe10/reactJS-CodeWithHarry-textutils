14b486786b78ded3471910408ba3374678930652
Utkarsh Yashwant Tambe <89902664+utkarshtambe10@users.noreply.github.com>
2022-10-31 23:53:37 +0530

    Update Alert.js

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Alert(props) {
     const capitalize = (word) => {
         const lower = word.toLowerCase();
-        return lower,charAt(0).toUpperCase() + lower.slice(1);
+        return lower.charAt(0).toUpperCase() + lower.slice(1);
     }
     return (
         <div style={{height: '50px'}}>
